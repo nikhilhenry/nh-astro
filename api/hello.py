b060dba2d2d14a8d1f825ad9a667e49f442596fb
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-05-31 10:05:50 +0530

    encode string

--- api/hello.py
+++ api/hello.py
@@ -7,5 +7,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write("hello world")
+    self.wfile.write(str("hello world").encode())
     return
