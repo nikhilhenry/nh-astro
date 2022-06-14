af56f42ebe7cfd456e75f74454cc9546713dbba2
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-14 11:36:55 +0530

    set theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,24 @@
+const colors = require("tailwindcss/colors");
+delete colors["lightBlue"]; // A bit hackful but fixes - https://github.com/tailwindlabs/tailwindcss/issues/4690
+delete colors["warmGray"];
+delete colors["trueGray"];
+delete colors["coolGray"];
+delete colors["blueGray"];
+
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  safelist: ["dark"],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      // Shades-of-X
+      // keep one group of colors enabled at a time
+      primary: colors.purple[700],
+      primaryDark: colors.purple[300],
+      primarySecondary: colors.purple[800],
+      primarySecondaryDark: colors.purple[500],
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
