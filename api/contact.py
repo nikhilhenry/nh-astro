8aafa37a6c9c1e64ae2f5766d2ca7ef6a23cc180
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-16 19:09:37 +0530

    delete api

--- api/contact.py
+++
@@ -1,9 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-from datetime import datetime
-
-class handler(BaseHTTPRequestHandler):
-
-  def do_POST(self):
-    self.send_response(200,"contacting...")
-    self.end_headers()
-    return
