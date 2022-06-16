8ea489716d3ccf8ff39b3e2261559c58b90cf194
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-16 20:31:46 +0530

    add protocol to url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "nikhilhenry.vercel.app",
+  site: "https://nikhilhenry.vercel.app",
   integrations: [svelte(), tailwind(), astroImageTools, sitemap()],
 });
