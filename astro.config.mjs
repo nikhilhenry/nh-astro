7025dd2056ff3ac5ee002ff056fa567c59d602fa
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-16 20:30:54 +0530

    generate site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), astroImageTools],
+  site: "nikhilhenry.vercel.app",
+  integrations: [svelte(), tailwind(), astroImageTools, sitemap()],
 });
