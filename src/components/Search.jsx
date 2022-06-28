5a005e0d6d75a0bc95ac3be263a1babda78e9c19
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-28 23:03:18 +0530

    add search

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import { DocSearch } from "@docsearch/react";
+
+import "@docsearch/css";
+
+function App() {
+  return (
+    <body className="dark">
+      <DocSearch
+        appId="E1VULT68ST"
+        apiKey="882d61bc857180f54ef5d19ec0b9100d"
+        indexName="nikhilhenry"
+      />
+    </body>
+  );
+}
+
+export default App;
