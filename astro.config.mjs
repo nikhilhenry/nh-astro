4af8a8e26b3d919207d7a6eef0459e221e47018b
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-05-29 17:27:53 +0530

    install mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nikhilhenry.vercel.app",
-  integrations: [svelte(), tailwind(), astroImageTools, sitemap()],
+  integrations: [svelte(), tailwind(), astroImageTools, sitemap(), mdx()]
 });
