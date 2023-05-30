bc590f518a0d26a83b73d8c0e03a08506047be74
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-05-30 09:30:44 +0530

    install react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nikhilhenry.vercel.app",
-  integrations: [svelte(), tailwind(), astroImageTools, sitemap(), mdx()]
+  integrations: [svelte(), tailwind(), astroImageTools, sitemap(), mdx(), react()]
 });
