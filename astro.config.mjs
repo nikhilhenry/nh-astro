7b129ebef940cda0fb689c90f72af36c75ab09a1
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-05-30 09:46:09 +0530

    install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nikhilhenry.vercel.app",
-  integrations: [svelte(), tailwind(), astroImageTools, sitemap(), mdx(), react()]
+  integrations: [svelte(), tailwind(), astroImageTools, sitemap(), mdx(), react(), preact()]
 });
